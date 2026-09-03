44d7cc55ad225a1d9e2d7675f183fdad7f38b696
SamspamIt <sanjanaa.s.official@gmail.com>
2026-09-03 17:55:46 +0530

    fix: point frontend to deployed backend URL

--- Frontend/src/api/axiosInstance.js
+++ Frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL:"http://localhost:5000/api",
+    baseURL:"https://blogsy-skv1.onrender.com/api",
     withCredentials: true,
 });
 
